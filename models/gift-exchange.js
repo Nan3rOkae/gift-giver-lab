72e96c17b77dcef1c25961bc5ee124f9637a235f
Nana Kofi Okae <okaen@vcu.edu>
2022-06-28 10:18:18 -0700

    step 3 done

--- models/gift-exchange.js
+++ models/gift-exchange.js
@@ -1,5 +1,66 @@
-const express = require("express");
-modelRouter = express.Router();
-class GiftExchange {}
+const { BadRequestError } = require("../utils/errors.js");
 
-module.exports = modelRouter;
+function getRandomInt(top) {
+  return Math.floor(Math.random() * (top + 1));
+}
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+class GiftExchange {
+  static pairs(names) {
+    if (names.length % 2 == 1)
+      throw new BadRequestError("Number of names cannot be odd");
+    var tuples = [];
+    var originalSize = names.length;
+    for (let i = 0; i < originalSize / 2; i++) {
+      let idxOne = getRandomInt(names.length - 1);
+      let nameOne = names[idxOne];
+      names.splice(idxOne, 1);
+
+      let idxTwo = getRandomInt(names.length - 1);
+      let nameTwo = names[idxTwo];
+      names.splice(idxTwo, 1);
+
+      tuples.push([nameOne, nameTwo]);
+    }
+
+    return tuples;
+  }
+
+  static traditional(names) {
+    let shuffledNames = shuffle(names);
+    console.log("out");
+
+    let response = [];
+
+    for (let i = 0; i < names.length; i++) {
+      if (i == names.length - 1) {
+        response.push(
+          `${shuffledNames[i]} is giving a gift to ${shuffledNames[0]}`
+        );
+        break;
+      }
+
+      response.push(
+        `${shuffledNames[i]} is giving a gift to ${shuffledNames[i + 1]}`
+      );
+    }
+
+    return response;
+  }
+}
+
+module.exports = GiftExchange;
